78a4d9312168bdb35747e87950f4b5536b38fbfd
BoiLMa <boil1@protonmail.com>
2023-05-05 17:37:48 +0200

    chest

--- Assets/Chest.cs
+++ Assets/Chest.cs
@@ -1,38 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Chest : MonoBehaviour
 {
-    public GameObject chestInstantiated;
+    public GameObject chestGameObject;
     public Item chestItem;
-    public Sprite s1;
-    public Sprite s2;
-    public Sprite s3;
     public List<Item> chestItems = new List<Item>() {
         new Item("Scythe"), new Item("Sword"), new Item("Axe"), 
         new Item("Holy Gun"), new Item("Long Ranged Gun"), 
         new Item("Short Ranged Gun"), new Item("Normal Ranged Gun"),
         new Item("Holy Bullets"), new Item("Bullets") };
     public int tier;
+    public bool open = false;
     public Chest(GameObject prefab, Vector3 position, int _tier)
     {
         chestItem = chestItems[Random.Range(0, chestItems.Count)];
         Debug.Log(chestItem.itemName);
         tier = _tier;
-        chestInstantiated = Object.Instantiate(prefab, position, Quaternion.identity);
-        //sprite ontzichtbaar
-        switch (tier)
-        {
-            case 0:
-                chestInstantiated.GetComponent<SpriteRenderer>().sprite = s1;
-                break;
-            case 1:
-                chestInstantiated.GetComponent<SpriteRenderer>().sprite = s2;
-                break;
-            case 2:
-                chestInstantiated.GetComponent<SpriteRenderer>().sprite = s3;
-                break;
-        }
+        chestGameObject = Object.Instantiate(prefab, position, Quaternion.identity);
     }
 }
--- Assets/Placeholder.cs
+++ Assets/Placeholder.cs
@@ -6,13 +6,60 @@ public class Placeholder : MonoBehaviour
 {
     public GameObject chest;
     public bool clearedRoom;
+    public Chest c;
+    public Sprite s1;
+    public Sprite s2;
+    public Sprite s3;
+    public Sprite s4;
     // Update is called once per frame
     void Update()
     {
+        Walk();
         if (clearedRoom)
         {
-            new Chest(chest, new Vector3(0, 0), Random.Range(0,3));
+            int tier = Random.Range(0, 100);
+            c = new Chest(chest, new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, 0), tier);
+            if (!c.open)
+            {
+                c.chestGameObject.transform.GetChild(0).gameObject.SetActive(false);
+                Debug.Log("setfalse");
+            }
+            switch (tier)
+            {
+                case < 10:
+                    c.chestGameObject.GetComponent<SpriteRenderer>().sprite = s1;
+                    c.tier = 3;
+                    break;
+                case < 25: //e
+                    c.chestGameObject.GetComponent<SpriteRenderer>().sprite = s2;
+                    c.tier = 2;
+                    break;
+                case < 50: //r
+                    c.chestGameObject.GetComponent<SpriteRenderer>().sprite = s3;
+                    c.tier = 1;
+                    break;
+                case < 100: //c
+                    c.chestGameObject.GetComponent<SpriteRenderer>().sprite = s4;
+                    c.tier = 0;
+                    break;
+            }
             clearedRoom = false;
         }
+        if (Input.GetKey(KeyCode.E)) //set active gebeurt niet
+        {
+            chest.transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+    void Walk()
+    {
+        int speed = 5;
+        if (Input.GetKey(KeyCode.W))
+            gameObject.transform.position += new Vector3(0, speed) * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+            gameObject.transform.position += new Vector3(-speed, 0) * Time.deltaTime;
+        if (Input.GetKey(KeyCode.S))
+            gameObject.transform.position += new Vector3(0, -speed) * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D))
+            gameObject.transform.position += new Vector3(speed, 0) * Time.deltaTime;
     }
 }
